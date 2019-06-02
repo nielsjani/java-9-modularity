c3a2320e43d2dfb78b1c851fdd5d9e239286311a
nielsjani <NielsJani>
2019-06-02 20:32:29 +0200

    implement 'read validator resources file' story

--- electioneer/passport-validation-internal/src/com/electioneer/passportvalidation/internal/PassportValidatorController.java
+++ electioneer/passport-validation-internal/src/com/electioneer/passportvalidation/internal/PassportValidatorController.java
@@ -3,7 +3,12 @@ package com.electioneer.passportvalidation.internal;
 
 import com.electioneer.passportvalidationapi.PassportValidator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ServiceLoader;
+import java.util.stream.Collectors;
 
 import static com.electioneer.passport.Passport.passport;
 
@@ -14,6 +19,7 @@ public class PassportValidatorController {
                                  String locationData,
                                  String personalData
     ) {
+        readInPropertiesFileFromApiModule();
         ServiceLoader.load(PassportValidator.class).forEach(validator -> {
             System.out.println("Validating with " + validator.getClass().getSimpleName());
             validator.validate(passport()
@@ -22,5 +28,23 @@ public class PassportValidatorController {
                     .withLocationData(locationData)
                     .withPersonalData(personalData));
         });
+    }
+
+    private void readInPropertiesFileFromApiModule() {
+        ModuleLayer.boot().findModule("com.electioneer.passportvalidationapi")
+                .ifPresentOrElse(
+                        res -> {
+                            try {
+                                InputStream resourceAsStream = res.getResourceAsStream("validator-settings.properties");
+                                String result = new BufferedReader(new InputStreamReader(resourceAsStream))
+                                        .lines().collect(Collectors.joining("\n"));
+                                System.out.println(" \n Result from reading in properties file from another module");
+                                System.out.println(result);
+                                System.out.println("\n");
+                            } catch (IOException e) {
+                                System.out.println("Found properties file module, but could not get properties file");
+                            }
+                        },
+                        () -> System.out.println("Not found :("));
     }
 }
